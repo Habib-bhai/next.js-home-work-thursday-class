7548a8839009217786c6006576994e4770e05b8c
Habib-bhai <habibahmed918131@gmail.com>
2024-10-08 23:48:10 +0500

    changes in heroSection image of "/"

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
     <>
       <Navbar />
       <div className="w-full h-screen flex md:flex-nowrap flex-wrap justify-between items-center">
-        <Image src={"/heroSection.jpg"} alt="home" width={850} height={500} className="opacity-60 md:h-[748px]" />
+        <Image src={"/heroSection.jpg"} alt="home" width={850} height={500} className="opacity-60 md:h-[568px]" />
         <div className="flex flex-col items-start justify-center h-full w-full p-8 bg-gray-300 text-black">
           <h3 className="font-bold text-lg">Made to last.</h3>
           <h1 className="font-bold text-4xl leading-tight mt-4">The new definition of deluxe.</h1>
